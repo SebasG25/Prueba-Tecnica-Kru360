02e334e7b4d32ee9ced3234e17cfd36dd4d81327
Sebastian Guzman <bitas200225@gmail.com>
2022-05-24 17:07:56 -0500

    Landing Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import './App.css'
-import { SearchBar } from './components/SearchBar/SearchBar'
-import { Contacts } from './components/ContactsList/Contacts'
-import { Button } from './components/Button/Button'
-import { MdOutlineAddCircle } from 'react-icons/md'
 import { LandingPage } from './pages/LandingPage'
 
 function App() {
