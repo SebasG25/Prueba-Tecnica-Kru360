29118df05f8b25c67c07555bcbb3719f8e2d99a0
Sebastian Guzman <bitas200225@gmail.com>
2022-05-24 21:05:09 -0500

    Contact list fixed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,17 +4,17 @@ import { IoMdTrash } from 'react-icons/io'
 import { useNavigate } from 'react-router-dom'
 
 export const Contact = ({ contact }) => {
-    const { name, number, prefix } = contact
+    const { name, lastname, number, prefix } = contact
     const navigate = useNavigate()
 
     return (
         <div className={styles.contactContainer}>
             <div className={styles.contactInfo}>
-                <h3 className={styles.contactName}>{name}</h3>
+                <h3 className={styles.contactName}>{name} {lastname}</h3>
                 <h3 className={styles.contactNumber}>{`(${prefix}) ${number}`}</h3>
             </div>
             <div className={styles.contactActions}>
-                <MdModeEditOutline onClick={() => navigate(`/edit/${name}`)} color='#066493' size={20} className={styles.actionIcon} />
+                <MdModeEditOutline onClick={() => navigate(`/edit/${name} ${lastname}`)} color='#066493' size={20} className={styles.actionIcon} />
                 <MdCheckCircle color='#066493' size={20} className={styles.actionIcon} />
                 <IoMdTrash color='#066493' size={20} className={styles.actionIcon} />
             </div>
